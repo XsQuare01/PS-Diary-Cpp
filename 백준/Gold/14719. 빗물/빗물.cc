1e59140eb3a7334a5a2b95bf51f3fdd6831f91bb
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-02-01 16:04:07 +0900

    [Gold V] Title: 빗물, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.cc
@@ -0,0 +1,62 @@
+#define MAX 500
+#define INF 60000000
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int h, w;
+int arr[MAX];
+int larr[MAX];
+int rarr[MAX];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> h >> w;
+    for(int i = 0; i < w; i++){
+        cin >> arr[i];
+    }
+
+    int st = min(arr[0], arr[w-1]);
+    
+    for(int i = st; i <= h; i++){
+        int left = 0;
+        int right = w-1;
+
+        for(int j = 0; j < w; j++){
+            if(arr[left] < arr[j]){
+                left = j;
+            }
+            if(arr[left] < i){
+                continue;
+            }
+            larr[j] = i;
+
+        }
+
+        for(int j = w - 1; j >= 0; j--){
+            if(arr[right] < arr[j]){
+                right = j;
+            }
+
+            if(arr[right] < i){
+                continue;
+            }
+            rarr[j] = i;
+        }
+    }
+
+    int ans = 0;
+    for(int i = 0; i < w; i++){
+        int res = min(larr[i], rarr[i]);
+        if(arr[i] >= res){
+            continue;
+        }
+        ans += res - arr[i];
+    }
+    
+    cout << ans;
+    return 0;
+}
