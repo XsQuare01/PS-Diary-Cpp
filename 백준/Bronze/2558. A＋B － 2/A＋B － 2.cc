648a7502dab70d647e5fa7a51dbf9263b9ab7186
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-04-20 23:02:53 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,20 @@
+#define MAX 1000
+#define KEY 3900
+#define INF 987654321
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int A, B;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> A >> B;
+    cout << A + B;
+
+    return 0;
+}
+ 
