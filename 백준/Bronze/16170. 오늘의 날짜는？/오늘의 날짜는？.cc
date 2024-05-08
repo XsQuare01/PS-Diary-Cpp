85dfd8c43241279c993fbb64f16e788d23a44194
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-05-08 15:45:57 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.cc
@@ -0,0 +1,16 @@
+#define MAX 2222
+#define INF 987654321
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); 
+
+    cout << "2024" << "\n";
+    cout << "05" << "\n";
+    cout << "08" << "\n";
+    return 0;
+}
