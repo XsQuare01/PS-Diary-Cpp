167e03d391185ff1d63f7c5f2e3180ef14142b10
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-05-23 13:35:50 +0900

    [Bronze V] Title: 身長 (Height), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.cc
@@ -0,0 +1,18 @@
+#define MAX 10000
+#define INF 987654321
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int A, B;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> A >> B;
+    cout << B - A;
+    
+    return 0;
+}
