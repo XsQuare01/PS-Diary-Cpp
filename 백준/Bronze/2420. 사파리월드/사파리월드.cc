311755cccd785b01c9eedc72b85d6d8cd34d0d7b
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-05-28 00:34:19 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,17 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+long long N, M;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> N >> M;
+    cout << abs(N - M);
+
+    return 0;
+}
+ 
