c2a8a4f75abaa780a01dbe574ff16833b01f6d52
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-06-02 15:20:51 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,19 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string s;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> s;
+    cout << ":fan::fan::fan:" <<"\n";
+    cout << ":fan::"<< s <<"::fan:" <<"\n";
+    cout << ":fan::fan::fan:" <<"\n";
+
+    return 0;
+}
+ 
