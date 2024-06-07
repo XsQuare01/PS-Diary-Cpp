594ec46f2f0f16eb2ebd116569c52ca5f171c5d9
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-06-07 21:59:53 +0900

    [Silver I] Title: 숫자 할리갈리 게임, Time: 32 ms, Memory: 2524 KB -BaekjoonHub

---
+++ 백준/Silver/20923. 숫자 할리갈리 게임/숫자 할리갈리 게임.cc
@@ -0,0 +1,141 @@
+#define WIN 5
+#define MAX 30000
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int N, M;
+int arr1[MAX];
+int arr2[MAX];
+int t1 = 0; int t2 = 0;
+queue<int> q1, q2, r1, r2;
+
+bool WinPlayer1(){
+    if(t1 == WIN || t2 == WIN){
+        // cout << t1 << " " << t2 << endl;
+        // cout << "1 Win" << endl;
+        return true;
+    }
+    return false;
+}
+
+bool WinPlayer2(){
+    if(!t1 || !t2){
+        return false;
+    }
+    if(t1 + t2 == WIN){
+        //Debug
+        // cout << t1 << " " << t2 << endl;
+        // cout << "2 Win" << endl;
+        return true;
+    }
+    return false;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> N >> M;
+    for(int i = 0; i < N; i++){
+        cin >> arr1[i] >> arr2[i];
+    }
+    for(int i = N-1; i >= 0; i--){
+        q1.push(arr1[i]);
+        q2.push(arr2[i]);
+    }
+    for(int i = 0; i < M; i++){
+        if(i % 2 == 0){
+            t1 = q1.front();
+            q1.pop();
+            r1.push(t1);
+
+            if(q1.empty()){
+                cout << "su";
+                return 0;
+            }
+
+            if(WinPlayer1()){
+                t1 = 0; t2 = 0;
+                while(!r2.empty()){
+                    int t = r2.front();
+                    r2.pop();
+                    q1.push(t);
+                }
+                while(!r1.empty()){
+                    int t = r1.front();
+                    r1.pop();
+                    q1.push(t);
+                }
+            }
+            if(WinPlayer2()){
+                t1 = 0; t2 = 0;
+                while(!r1.empty()){
+                    int t = r1.front();
+                    r1.pop();
+                    q2.push(t);
+                }
+                while(!r2.empty()){
+                    int t = r2.front();
+                    r2.pop();
+                    q2.push(t);
+                }
+            }
+        }
+        else{
+            t2 = q2.front();
+            q2.pop();
+            r2.push(t2);
+
+            if(q2.empty()){
+                cout << "do";
+                return 0;
+            }
+            if(WinPlayer1()){
+                t1 = 0; t2 = 0;
+                while(!r2.empty()){
+                    int t = r2.front();
+                    r2.pop();
+                    q1.push(t);
+                }
+                while(!r1.empty()){
+                    int t = r1.front();
+                    r1.pop();
+                    q1.push(t);
+                }
+            }
+            if(WinPlayer2()){
+                t1 = 0; t2 = 0;
+                while(!r1.empty()){
+                    int t = r1.front();
+                    r1.pop();
+                    q2.push(t);
+                }
+                while(!r2.empty()){
+                    int t = r2.front();
+                    r2.pop();
+                    q2.push(t);
+                }
+            }
+        }
+    }
+
+    // cout << q1.size() << " " << q2.size() << "\n";
+
+    if(q1.size() > q2.size()){
+        cout << "do";
+        return 0;
+    }
+    if(q1.size() < q2.size()){
+        cout << "su";
+        return 0;
+    }
+    if(q1.size() == q2.size()){
+        cout << "dosu";
+        return 0;
+    }
+
+    return 0;
+}
+ 
