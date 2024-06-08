56d9c311e8205b71f8e220082e582ae7453927a6
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-06-08 11:34:46 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+long long R;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    for(int i = 0; i < 5; i++){
+        long long t;
+        cin >> t;
+        R += t;
+    }
+    cout << R;
+
+    return 0;
+}
+ 
