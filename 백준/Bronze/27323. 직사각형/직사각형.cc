fad5f51ec708e7aa9821dc2719d169b542646a3d
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-06-12 00:22:50 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,19 @@
+#define WIN 5
+#define MAX 30000
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int A, B;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> A >> B;
+    cout << A * B;
+
+    return 0;
+}
+ 
