b482b5bdbd5279dd7a6a83b636bd249f4dff04b4
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-06-25 13:45:28 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,20 @@
+#define MAX 50
+#define LEN 50
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string s;
+int N;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> s >> N;
+    cout << s[N-1];
+
+    return 0;
+}
+ 
