9a1dd0c29379b387fa6068c80231bd5a780b929d
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-06-29 23:57:59 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,19 @@
+#define MAX 50
+#define LEN 50
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+long long s1, s2;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> s1 >> s2;
+    cout << (s1 == s2) ? 1 : 0;
+
+    return 0;
+}
+ 
