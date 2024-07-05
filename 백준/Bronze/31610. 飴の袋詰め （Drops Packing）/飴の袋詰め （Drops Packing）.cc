571dd5cde87bcc3ce34965e7ca50655cd7803a7d
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-07-05 13:12:25 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.cc
@@ -0,0 +1,21 @@
+#define MIN 1
+#define MAX 26
+#define LEN 5000000
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int A, B, C;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> A >> B >> C;
+    cout << A*B+C;
+
+    
+    return 0;
+}
+ 
