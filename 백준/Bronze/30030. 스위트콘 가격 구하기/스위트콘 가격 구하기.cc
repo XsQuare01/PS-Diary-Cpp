697d2c654e538219d632370e7863c22dbee7ae97
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-07-07 00:38:57 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,21 @@
+#define MIN 1
+#define MAX 26
+#define LEN 5000000
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int A;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> A;
+    cout << A / 11 * 10;
+
+    
+    return 0;
+}
+ 
