37289d858805a299522ddfe1a4499e769e5ccda7
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-07-15 14:43:06 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,17 @@
+#define MAX 100
+#define INF 2147483647
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> N;
+    cout << N - 1946;
+    return 0;
+}   
