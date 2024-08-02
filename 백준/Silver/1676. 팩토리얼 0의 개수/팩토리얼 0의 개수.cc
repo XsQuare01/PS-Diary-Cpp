d64e0ebf4b4e70629c5c771be93321e8b920acf4
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-08-02 16:21:19 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -0,0 +1,26 @@
+#define MAX 100
+#define INF 1000000000
+#define PI 3.141592653589793
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> varr;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    for(int i = 5; i <= INF; i *= 5){
+        varr.push_back(i);
+    }
+
+    int n, res = 0;   
+    cin >> n;
+    for(auto a: varr){
+        res += (n / a);
+    }
+    cout << res << "\n";
+    return 0;
+}
