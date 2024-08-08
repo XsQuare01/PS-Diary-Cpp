6477d2683661006284f35e1ce34ff1f002e0359f
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-08-08 13:46:53 +0900

    [Unrated] Title: 구구, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/9999. 구구/구구.cc
@@ -0,0 +1,19 @@
+#define MAX 10
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int arr[MAX];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout <<"Hymn To Love";
+    // "the Hymn to Love" 
+    // "Hymne A L'Amour"  
+
+    return 0;
+}
