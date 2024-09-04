4c811ed51dd57c241547a015c8140b515af9c715
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-09-04 23:08:10 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.cc
@@ -0,0 +1,20 @@
+#define MIN 1
+#define MAX 1000000
+#define LEN 5000000
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int A;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> A;
+    cout << ceil(1.0 * A / 5);
+
+    return 0;
+}
+ 
