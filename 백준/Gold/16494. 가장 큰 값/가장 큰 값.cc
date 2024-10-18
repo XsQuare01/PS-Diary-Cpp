50f2e83200eb3469a2b25aa9be520603b43de897
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-10-18 15:59:05 +0900

    [Gold V] Title: 가장 큰 값, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/16494. 가장 큰 값/가장 큰 값.cc
+++ 백준/Gold/16494. 가장 큰 값/가장 큰 값.cc
@@ -1,73 +1,86 @@
-#include <bits/stdc++.h>
+#define MAX 20
+#define MOD 1000000007
+#define INF 10000000
+
+#include<bits/stdc++.h>
+
 using namespace std;
-#define TEST int tt; cin >> tt; while (tt--)
-#define all(v) (v).begin(), (v).end()
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-#define tii tuple<int, int, int>
-#define ll long long
-#define FASTIO ios_base::sync_with_stdio(false); cin.tie(0);
-const int MAX = 200'005;
-const int INF = 1e9 + 1;
-const ll MOD = 1'000'003;
-
-int num[50];
-int sum[50][50];
-int dp[40][40];
 
 
-int main() {
-    FASTIO
-    int N, M;
+int N, M, res = -INF;
+int arr[MAX];
+int parr[MAX][MAX];
+int dp[MAX][MAX];
+vector<int> perm;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     cin >> N >> M;
-    for(int i=1; i<=N; i++){
-        cin >> num[i];
+    for(int i = 0; i < N; i++){
+        cin >> arr[i];
     }
-    for(int i=1; i<=N; i++){
-        for(int j=i; j<=N; j++)dp[i][j] = -INF;
-    }
-    vector<int> v;
-    for(int i=0; i<N; i++){
-        for(int j=1; j<=N; j++){
-            if(j+i>N)break;
-            sum[j][j+i] = sum[j][j+i-1] + num[j+i];
+
+    for(int i = 0; i < N; i++){
+        parr[i][i] = arr[i];
+        for(int j = i+1; j < N; j++){
+            parr[i][j] = parr[i][j-1] + arr[j];
         }
     }
-    for(int i=1; i<=N; i++){
-        for(int j=i; j<=N; j++){
-            for(int a=i; a<=j; a++){
-                for(int b=a; b<=j; b++){
-                    dp[i][j] = max(dp[i][j], sum[a][b]);
+
+    //Debug
+    
+
+    for(int i = 0; i < N; i++){
+        for(int j = i; j < N; j++){
+            dp[i][j] = -INF;
+        }
+    }
+
+    for(int i = 0; i < N; i++){
+        for(int j = i; j < N; j++){
+            for(int k = i; k <= j; k++){
+                for(int l = k; l <= j; l++){
+                    dp[i][j] = max(dp[i][j], parr[k][l]);
                 }
             }
         }
     }
-    vector<int> a;
-    vector<int> b;
-    int cnt = 0;
-    for(int i=1; i<=N; i++){
-        a.emplace_back(i);
-        if(cnt<M-1){
-            b.emplace_back(1);
-            cnt++;
-        }
-        else b.emplace_back(0);
+
+    for(int i = 0; i < M-1; i++){
+        perm.push_back(1);
     }
-    int before;
-    int ans = -INF;
-    do {
-        int sum =0;
-        before = 1;
-        for (int i = 0; i < a.size(); ++i) {
-            if (b[i] == 1){
-                sum += dp[before][a[i]];
-                before = a[i]+1;
+    for(int i = M-1; i < N-1; i++){
+        perm.push_back(0);
+    }
+
+    if(N == 1){
+        perm.push_back(0);
+    }
+
+    res = -INF;
+    do{
+        int tsum = 0;
+        int prev = 0;
+        for(int i = 0; i < N; i++){
+            if(!perm[i]){
+                continue;
             }
+
+            tsum += dp[prev][i];
+            prev = i+1;
         }
-        if(before<=N){
-            sum+=dp[before][N];
-            ans = max(ans, sum);
+
+        if(prev < N){
+            tsum += dp[prev][N-1];
         }
-    } while (prev_permutation(b.begin(), b.end()));
-    cout << ans;
+
+        res = max(res, tsum);
+
+    }while(prev_permutation(perm.begin(), perm.end()));
+
+
+    cout << res;
+    return 0;
 }
