0b2a0bab57c00c2132d9283aecddfa682e42664a
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-10-23 15:14:52 +0900

    [Gold IV] Title: 승리하라, Time: 40 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/31862. 승리하라/승리하라.cc
+++ 백준/Gold/31862. 승리하라/승리하라.cc
@@ -8,10 +8,39 @@ using namespace std;
 
 int N, M, K;
 int res = 0;
-int maximum = -1;
+int len = 0;
 int arr[MAX + 1];
-int sarr[MAX + 1];
 vector<pair<int, int>> vec;
+
+pair<int, int> checkWinner(pair<int, int> team, int idx){
+    if(arr[team.first] < arr[idx]){
+        team.second = team.first;
+        team.first = idx;
+    }
+    else if(arr[team.second] < arr[idx]){
+        team.second = idx;
+    }
+
+    return team;
+}
+
+void DFS(int cnt, pair<int, int> team){
+    if(cnt == len){
+        if(team.first == K && (arr[team.first] > arr[team.second] || team.first == team.second)){
+            res++;
+        }
+        return ;
+    }
+
+    auto [t1, t2] = vec[cnt];
+    arr[t1]++;
+    DFS(cnt+1, checkWinner(team, t1));
+    arr[t1]--;
+
+    arr[t2]++;
+    DFS(cnt+1, checkWinner(team, t2));
+    arr[t2]--;
+}
 
 int main(){
     ios::sync_with_stdio(false);
@@ -31,82 +60,9 @@ int main(){
             arr[t2]++;
         }
     }
+    len = vec.size();
 
-    // 해당 승 수를 가진 사람이 몇명 있는가?
-    for(int i = 1; i <= N; i++){
-        int idx = arr[i];
-
-        maximum = max(maximum, idx);
-        sarr[idx]++;
-    }
-    int len = vec.size();
-    if(len == 0){
-        if(sarr[maximum] == 1 && arr[K] == maximum){
-            res = 1;
-        }
-        cout << res;
-        return 0;
-    }
-
-    for(int i = 0; i < (1 << len); i++){
-        bitset<INF> switch_bitset(i);
-        unordered_map<int, int> tmap;
-        for(int j = 0; j < len; j++){
-            int t1 = vec[j].first;
-            int t2 = vec[j].second;
-
-            if(switch_bitset[j]){
-                if(tmap.find(t1) != tmap.end()){
-                    int cnt = tmap[t1];
-                    tmap[t1] = cnt+1;
-                }
-                else{
-                    tmap[t1] = 1;
-                }
-            }
-            else{
-                if(tmap.find(t2) != tmap.end()){
-                    int cnt = tmap[t2];
-                    tmap[t2] = cnt+1;
-                }
-                else{
-                    tmap[t2] = 1;
-                }
-            }
-        }
-
-        int t_max = maximum;
-        for(auto [team, score]: tmap){
-            int prev_index = arr[team];
-            sarr[prev_index]--;
-
-            int cur_index = arr[team] + score;
-            sarr[cur_index]++;
-
-            t_max = max(t_max, cur_index);
-        }
-        
-
-        if(sarr[t_max] == 1){
-            int t_k = 0;
-            if(tmap.find(K) != tmap.end()){
-                t_k = tmap[K];
-            }
-
-            if(arr[K] + t_k == t_max){
-                res++;
-            }
-        }
-
-        for(auto [team, score]: tmap){
-            int cur_index = arr[team] + score;
-            sarr[cur_index]--;
-
-            int prev_index = arr[team];
-            sarr[prev_index]++;
-        }
-    }
-
+    DFS(0, {K, 0});
     cout << res;
 
     return 0;
