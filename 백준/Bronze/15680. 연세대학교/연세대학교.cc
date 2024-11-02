10d1be202a7022d58d91decbebc7ab565294c5ee
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-11-02 15:08:04 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,29 @@
+#define MAX 64
+#define MOD 1000000007
+#define INF 1844674407370955161
+#define W 40
+#define WT 4
+#define H 8
+#define HT 2
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+long long T, N;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> T;
+    if(T){
+        cout << "Leading the Way to the Future";
+    }
+    else{
+        cout << "YONSEI";
+    }
+
+    return 0;
+}
