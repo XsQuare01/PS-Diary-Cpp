1e4ee0a37fc074ec173dec79847b86adcd6389f8
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-11-05 15:32:08 +0900

    [Gold IV] Title: 효율적인 애니메이션 감상, Time: 24 ms, Memory: 7596 KB -BaekjoonHub

--- 백준/Gold/27313. 효율적인 애니메이션 감상/효율적인 애니메이션 감상.cc
+++ 백준/Gold/27313. 효율적인 애니메이션 감상/효율적인 애니메이션 감상.cc
@@ -18,9 +18,8 @@ int main(){
     int N, M, K;
     cin >> N >> M >> K;
 
-    vector<int> list; // M 이하의 애니메이션을 저장할 리스트
+    vector<int> list;
 
-    // 애니메이션 시간 입력 및 M 이하의 시간만 저장
     for (int i = 0; i < N; i++) {
         int t;
         cin >> t;
@@ -28,33 +27,30 @@ int main(){
             list.push_back(t);
     }
 
-    // 오름차순 정렬
     sort(list.begin(), list.end());
 
     if (list.empty()) {
-        cout << "0\n"; // 애니메이션이 존재하지 않을 때
+        cout << "0\n";
     } else {
-        // 우선순위 큐 (최소 힙)로 현재 시청 중인 애니메이션 저장
         priority_queue<int, vector<int>, greater<int>> pq;
-        pq.push(list[0]); // 첫 번째 애니메이션 추가
+        pq.push(list[0]);
         int answer = 1;
 
-        // 2번째 애니메이션부터 시청 시작
         for (size_t i = 1; i < list.size(); i++) {
-            if (pq.size() == K) { // 우선순위 큐 크기 == K
-                if (pq.top() + list[i] > M) // M을 초과하면 시청 불가
+            if (pq.size() == K) {
+                if (pq.top() + list[i] > M)
                     break;
                 else {
-                    pq.push(pq.top() + list[i]); // M 이하이면 시청
+                    pq.push(pq.top() + list[i]);
                     pq.pop();
                 }
-            } else { // 우선순위 큐 크기 < K
-                pq.push(list[i]); // 애니메이션 시청
+            } else {
+                pq.push(list[i]);
             }
             answer++;
         }
 
-        cout << answer << "\n"; // 시청한 애니메이션 출력
+        cout << answer << "\n";
     }
 
     return 0;
