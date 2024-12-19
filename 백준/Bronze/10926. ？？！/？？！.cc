3650b23174dafe883a6064517b015ba38da347d4
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-12-19 21:11:47 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define MAX 100000
+#define INF 2100000000
+#define MOD 998244353
+
+using namespace std;
+
+string S;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> S;
+    cout << S + "??!";
+
+
+    return 0;
+}
