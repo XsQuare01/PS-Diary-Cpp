ef01f13cc4a34f31db922f594cc18a35f213f7c4
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2024-12-27 22:42:21 +0900

    [Bronze V] Title: AI 선도대학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.cc
@@ -0,0 +1,19 @@
+#define MIN 1
+#define MAX 1000000
+#define LEN 5000000
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int N;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> N;
+    cout << N - 2024;
+    return 0;
+}
+ 
