3a8d4980f19565a10c497de5c7689e562efa6a95
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2025-02-09 20:05:56 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,20 @@
+#define MAX 2000
+#define TMAX 300000
+#define INF 2100000000
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long A, B, C;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> A >> B >> C;
+	cout << A + B + C;
+	
+	return 0;
+}
