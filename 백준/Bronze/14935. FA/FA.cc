befa458ee4580ef1583f9868c9f94edace4b41cb
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2025-02-21 21:23:55 +0900

    [Bronze IV] Title: FA, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14935. FA/FA.cc
@@ -0,0 +1,19 @@
+#define MAX 1000
+#define TMAX 300000
+#define INF 2100000000
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string S;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> S;
+	cout << "FA";
+	return 0;
+}
