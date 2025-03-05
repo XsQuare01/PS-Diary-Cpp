39cbebf1830e29cf4768d289036c1a2fd99829e2
XsQuare01 <48215456+XsQuare01@users.noreply.github.com>
2025-03-05 23:24:00 +0900

    [Bronze V] Title: HI-ARC=?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.cc
@@ -0,0 +1,22 @@
+#define MIN 1
+#define MAX 1000001
+#define LEN 5000000
+#define INF 987654321
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int a, b, c, d, e;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> a >> b >> c >> d >> e;
+    cout << a * b - c * d * e;
+
+
+	return 0;
+}
